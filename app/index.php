2806bd233b1add05c89c4d65845d3e03f5a6f65c
pfinorbi <84071132+pfinorbi@users.noreply.github.com>
2024-04-01 23:01:02 +0200

    finalize workflow, disable test-connection pod in chart, make it possible to deactivate banner in app

--- app/index.php
+++ app/index.php
@@ -98,9 +98,17 @@
         <!--==========Slider area==========-->
         <section class="slider_area row m0">
             <div class="slider_inner">
+            <?php
+            $bannerEnabled = getenv('ENABLE_BANNER');
+
+            if ($bannerEnabled == "true") {
+                echo <<<EOL
                 <div class="banner">
                     <h2 class="banner" style="visibility: visible;">Site wide 15% discount only today!</h2>
                 </div>
+                EOL;
+            }     
+            ?>
                 <div class="camera_caption">
                     <h2 class="wow fadeInUp animated">Make Your Shopping Easy</h2>
                     <h5 class="wow fadeIn animated" data-wow-delay="0.3s">Find everything accordingly</h5>
